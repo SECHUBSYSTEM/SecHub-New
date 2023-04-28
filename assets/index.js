66466f348d04368baaf865b47162cdd1b9e1c7b0
Faithful Marvellous Chibuikem <106301523+SECHUBSYSTEM@users.noreply.github.com>
2023-04-28 21:04:53 +0100

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,47 @@
+import airbnb from "./airbnb.png";
+import binance from "./binance.png";
+import coinbase from "./coinbase.png";
+import logo from "./SECHUBFULLLOGOTRANSPARENT.png";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import google from "./google.svg";
+import moveDown from "./moveDown.svg";
+import toggleClose from "./toggle-close.svg";
+import open from "./close.png";
+import apple from "./apple.svg";
+import sechubReveal from "./reveal.svg";
+import rightArrow from "./right-arrow.png";
+import arrowUp from "./arrow-up.svg";
+import facebook from "./facebook.svg";
+import nextArrow from "./next.png";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import toggleDown from "./toggleDown.svg";
+import mobApp from "./mob-app-dev.svg";
+import lady from "./laptop-lady.svg";
+
+export {
+  airbnb,
+  binance,
+  coinbase,
+  rightArrow,
+  nextArrow,
+  logo,
+  sechubReveal,
+  moveDown,
+  mobApp,
+  send,
+  toggleClose,
+  toggleDown,
+  open,
+  lady,
+  shield,
+  star,
+  google,
+  apple,
+  arrowUp,
+  facebook,
+  linkedin,
+  twitter,
+};
